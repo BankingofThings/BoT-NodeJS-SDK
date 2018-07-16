b77d046ab22a72d4875d6dde18f92511c8f093e0
nanchauha <31957102+nanchauha@users.noreply.github.com>
2018-07-16 11:47:37 +0200

    Remove test data (#2)

--- bot/communication.js
+++ bot/communication.js
@@ -2,15 +2,14 @@
 
 var Utils = require('./utils');
 const querystring = require('querystring');
-const https = require('http');
+const https = require('https');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 
 const baseURL = 'dev.bankingofthings.io';
-const baseURL_T = '127.0.0.1';
-const PORT = 8080;
+const PORT = 443;
 const ENDPOINT = 'bot_iot';
-const URL = baseURL_T;
+const URL = baseURL;
 
 const SSLFINGERPRINT = [
   '98:67:D8:29:37:E3:8C:2D:44:D5:C4:21:4B:D7:CB:DF:59:7A:CE:61'
