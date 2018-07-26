453efc9fb87482f7f336754d3e55e2b5a97b3a2b
Marcel <marcel@afterimage.nl>
2018-07-26 18:23:13 +0200

    Minor changes as requested by @coenmooij

--- bot/bot.js
+++ bot/bot.js
@@ -8,19 +8,7 @@ const BotConfig = require('./botConfiguration');
 
 let registrationLevel = Utils.getValueForKey('regLvl');
 
-/*
-    Store makerID (overwriting any previous value) if correctly passed as command line parameter:
-*/
-// 	TODO: create more "generic" solution if *additional* parameters are possible in the future!
-let argCount = process.argv.length;
-if (argCount > 2 && argCount < 5){
-	if (argCount === 4 && process.argv[2] === '--makerID'){
-		Utils.storeMakerID(process.argv[3]); // Alternatively, simply use setValueForKey directly?
-	} else {
-		console.log('Usage: node bot.js --makerID REPLACE-WITH-OWN-MAKERID');
-	}
-}
-
+Utils.processCommandLine(process.argv);
 Utils.hasMakerID();
 
 if (!registrationLevel && !Utils.botID()) {
--- bot/utils.js
+++ bot/utils.js
@@ -32,16 +32,10 @@ methods.botID = function() {
 	return methods.getValueForKey('botID');
 };
 
-methods.storeMakerID = function(data){
-	methods.setValueForKey('makerID', data);
-}
-
 methods.hasMakerID = function() {
     if (methods.makerID() === undefined) {
-        console.log('Your personalised makerID has not been set.');
-		console.log('To set it, bot.js should be run the FIRST time as follows:\n');
-        console.log('node bot.js --makerID REPLACE-WITH-OWN-MAKERID');
-        process.exit(2);
+		console.log('Your personalised makerID has not been set.\nTo set it, bot.js should be run the FIRST time as follows:\n\n\tnode bot.js --makerID REPLACE-WITH-OWN-MAKERID');
+		process.exit(2);
     }
 };
 
@@ -59,5 +53,38 @@ methods.initializeBoT = function() {
     methods.setValueForKey('publicKey', publicKey);
     methods.setValueForKey('botPrvkey', pair.private);
 };
+
+/*
+    Process command line parameters, if correctly passed:
+	 
+		node bot.js --key value
+		
+	(Fixed format for now: 1 key followed by 1 value)	
+*/
+methods.processCommandLine = function(cmdLine) {
+	const CL_NO_ARGS = 2;
+	const CL_TOO_MANY_ARGS = 5;
+	const CL_KEY_AND_VALUE = 4;
+	const CL_KEY_POS = 2;
+	const CL_VALUE_POS = 3;
+	
+	let argCount = cmdLine.length;
+	if (argCount > CL_NO_ARGS && argCount < CL_TOO_MANY_ARGS) {
+		if (argCount === CL_KEY_AND_VALUE){
+			switch (cmdLine[CL_KEY_POS]) {
+				case '--makerID':
+					methods.setValueForKey('makerID', cmdLine[CL_VALUE_POS]);
+					break;
+				default:
+					console.log('Key not recognized.');
+					process.exit(2);
+					break;
+			}		
+		} else {
+			console.log('Usage: node bot.js --key value');
+			process.exit(2);
+		}
+	}	
+}
 
 module.exports = methods;
