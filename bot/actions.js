ca0c54231fb7cdbc7cc46155e5ab52596e925310
Marcel <marcel@Marcels-MacBook-Pro.local>
2018-08-03 14:21:12 +0200

    Payload value optional

--- bot/actions.js
+++ bot/actions.js
@@ -107,7 +107,12 @@ methods.startServer = function() {
             });
             req.on('end', function() {
                 let payload = JSON.parse(body);
-                let value = payload.value;
+
+                if (payload.value) {
+                  let value = payload.value;
+                } else {
+                  let value = 0;
+                }
 
                 let action = validActionFor(payload);
 
