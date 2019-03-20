5a58f13ce4a90f94b20671eb1d3841c5d8ebf9ab
Pim Stolk <pim@bankingofthings.io>
2019-03-20 09:27:36 +0100

    reorder the config

--- bin/config.sh
+++ bin/config.sh
@@ -27,8 +27,8 @@ fi
 if [ "${multipair}" == "yes" ] ; then
 	echo -n "Enter your alternativeID: " 
 	read alternativeID
-	cd $DIR && make config key=multipair value=multipair
 	cd $DIR && make config key=aid value=$alternativeID
+	cd $DIR && make config key=multipair value=multipair
 fi
 
 
